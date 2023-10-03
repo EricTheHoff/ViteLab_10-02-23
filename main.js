effbef1bbc1b19094172cc826e58da8e02c80b29
Eric Hoffman <eric.peter.hoffman@gmail.com>
2023-10-03 09:05:15 -0600

    Secondary Commit

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 import './style.css';
+import getRandomWord from './src/randomWord.js';
+import setSharkImage from './src/sharkImage.js';
+import { setupWord } from './src/word.js';
+const word = getRandomWord()
 
 document.querySelector('#app').innerHTML = `
   <section id="shark-img"></section>
@@ -12,10 +16,14 @@ document.querySelector('#app').innerHTML = `
 
 const initSharkwords = () => {
   let numWrong = 0;
-  const word = 'word';
+  // const word = getRandomWord()
 
   // for debugging:
   console.log(`[INFO] Correct word is: ${word}`);
+  setSharkImage(document.querySelector(`#shark-img`), numWrong)
+  setupWord(word, document.querySelector('#word-container'))
 };
 
 initSharkwords();
+console.log(word)
+// setupWord(word, document.querySelector('#word-container'))
--- src/word.js
+++ src/word.js
@@ -1,7 +1,14 @@
 let word;
 
-function setupWord(initWord, element) {}
+function setupWord(initWord, element) {
+    word = initWord
+    word.split('').forEach(() => {
+        element.insertAdjacentHTML('beforeend', `<div class="letter-box"></div>`)
+    })
+}
 
 function isLetterInWord(letter) {}
 
 function revealLetterInWord(letter) {}
+
+export {setupWord}
